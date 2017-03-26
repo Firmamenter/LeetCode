f23b16d284f63bdd716327eabf7f30a4b6416d3b
dachen <2672921697@qq.com>
2017-03-25 20:56:43 -0400

    387

--- HashTable/387-First Unique Character in a String.java
+++ HashTable/387-First Unique Character in a String.java
@@ -1,4 +1,6 @@
-/*
+/**
+387. First Unique Character in a String
+
 Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
 
 Examples:
@@ -8,6 +10,8 @@ return 0.
 
 s = "loveleetcode",
 return 2.
+
+Solution: HashTable. indexOf() and lastIndexOf()
 */
 
 /*************************** my solution ***************************/
