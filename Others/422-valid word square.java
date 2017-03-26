653c71460e702bb5d308f0a4457cfbc279e97ce0
dachen <2672921697@qq.com>
2017-03-25 22:16:08 -0400

    422

--- Others/422-valid word square.java
+++ Others/422-valid word square.java
@@ -1,3 +1,17 @@
+/**
+422. Valid Word Square
+
+Given a sequence of words, check whether it forms a valid word square.
+
+A sequence of words forms a valid word square if the kth row and column read the exact same string, where 0 ≤ k < max(numRows, numColumns).
+
+Note:
+The number of words given is at least 1 and does not exceed 500.
+Word length will be at least 1 and does not exceed 500.
+Each word contains only lowercase English alphabet a-z.
+
+*/
+
 public class Solution {
     public boolean validWordSquare(List<String> words) {
         if(words == null || words.size() == 0){
