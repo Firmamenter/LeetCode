c3c52f5db44bd937401b96107aa3f0916422a2b2
dachen <2672921697@qq.com>
2017-03-25 20:49:11 -0400

    349

--- HashTable/349-Intersection of Two Arrays.java
+++ HashTable/349-Intersection of Two Arrays.java
@@ -1,4 +1,4 @@
-/*
+/**
 Given two arrays, write a function to compute their intersection.
 
 Example:
@@ -7,6 +7,8 @@ Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
 Note:
 Each element in the result must be unique.
 The result can be in any order.
+
+Solution: HashSet.
 */
 
 public class Solution {
