57bbd4c0d2862fa1f8317b580847d4950c3458db
dachen <2672921697@qq.com>
2017-03-25 21:37:57 -0400

    266

--- HashTable/266-Palindrome Permutation.java
+++ HashTable/266-Palindrome Permutation.java
@@ -1,8 +1,12 @@
-/*
+/**
+266. Palindrome Permutation
+
 Given a string, determine if a permutation of the string could form a palindrome.
 
 For example,
 "code" -> False, "aab" -> True, "carerac" -> True.
+
+Solution: HashSet.
 */
 
 public class Solution {
