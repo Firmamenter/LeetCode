3734245b8a8961745b7e5d2c749204efe5d2197a
dachen <2672921697@qq.com>
2017-05-31 16:28:22 -0400

    sqrt

---
+++ Binary Search/69-Sqrt(x).java
@@ -0,0 +1,35 @@
+/**
+69. Sqrt(x)
+
+Implement int sqrt(int x).
+
+Compute and return the square root of x.
+
+Solution: BS, but be careful about overflow.
+*/
+
+public class Solution {
+    public int mySqrt(int x) {
+        if (x < 0) {
+            return -1; 
+        }
+        long start = 0; 
+        long end = x; 
+        long mid; 
+        while (start + 1 < end) {
+            mid = start + (end - start) / 2; 
+            if (mid*mid == x) {
+                return (int)mid; 
+            } else if (mid*mid < x) {
+                start = mid; 
+            } else {
+                end = mid; 
+            }
+        }
+        if (end*end <= x) {
+            return (int)end; 
+        } else {
+            return (int)start; 
+        }
+    }
+}
