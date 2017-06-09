1e180191820824bfd4552c1c86656b0f957ae324
dachen <2672921697@qq.com>
2017-06-09 14:02:40 -0400

    285. Inorder Successor in BST

---
+++ Binary Search Tree/285-Inorder Successor in BST.java
@@ -0,0 +1,52 @@
+/**
+285. Inorder Successor in BST
+
+Given a binary search tree and a node in it, find the in-order successor of that node in the BST.
+
+Note: If the given node has no in-order successor in the tree, return null.
+
+Solution: InOrder Traversal using stack.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        if (root == null || p == null) {
+            return null; 
+        }
+        Stack<TreeNode> stack = new Stack<>(); 
+        TreeNode cur = root; 
+        stack.push(root); 
+        boolean triger = false; 
+        
+        while (cur != null || !stack.empty()) {
+            while (cur != null) {
+                if (cur.left != null) {
+                    stack.push(cur.left); 
+                }
+                cur = cur.left; 
+            }
+            if (triger) {
+                return stack.pop(); 
+            }
+            cur = stack.pop(); 
+            if (cur == p) {
+                triger = true; 
+            }
+            if (cur.right != null) {
+                stack.push(cur.right); 
+            }
+            cur = cur.right; 
+        }
+        
+        return null; 
+    }
+}
