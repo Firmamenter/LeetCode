c3cabd9d395f2ee4c62d3921b8e2e9fece2ee594
dachen <2672921697@qq.com>
2017-06-21 11:55:22 -0400

    21. Merge Two Sorted Lists

--- Linked List/21-Merge Two Sorted Lists.java
+++ Linked List/21-Merge Two Sorted Lists.java
@@ -2,6 +2,8 @@
 21. Merge Two Sorted Lists
 
 Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+
+Solution: Recursive.
 */
 
 /**
@@ -16,36 +18,19 @@ public class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1 == null) {
             return l2; 
-        }
-        if (l2 == null) {
+        } else if (l2 == null) {
             return l1; 
         }
-        ListNode header = null;
-        if (l1.val <= l2.val) {
-            header = l1;
-            l1 = l1.next;
+        
+        ListNode head; 
+        if (l1.val < l2.val) {
+            head = l1; 
+            l1.next = mergeTwoLists(l1.next, l2); 
         } else {
-            header = l2; 
-            l2 = l2.next; 
+            head = l2; 
+            l2.next = mergeTwoLists(l1, l2.next); 
         }
-        ListNode ite = header;
-        while (l1 != null && l2 != null) {
-            if (l1.val <= l2.val) {
-                ite.next = l1;
-                ite = ite.next; 
-                l1 = l1.next;
-            } else {
-                ite.next = l2; 
-                ite = ite.next; 
-                l2 = l2.next; 
-            }
-        }
-        if (l1 != null) {
-            ite.next = l1; 
-        }
-        if (l2 != null) {
-            ite.next = l2; 
-        }
-        return header; 
+        
+        return head; 
     }
 }
