0b56dbcdd44d03761edcc0fe9776c678c4477bb5
dachen <2672921697@qq.com>
2017-06-27 17:25:27 -0400

    206. Reverse Linked List

---
+++ Linked List/206-Reverse Linked List.java
@@ -0,0 +1,33 @@
+/**
+206. Reverse Linked List
+
+Reverse a singly linked list.
+
+Solution: Use a temporary ListNode to store the next node of head.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head; 
+        }
+        
+        ListNode pre = null; 
+        while (head != null) {
+            ListNode temp = head.next; 
+            head.next = pre; 
+            pre = head; 
+            head = temp; 
+        }
+        
+        return pre; 
+    }
+}
