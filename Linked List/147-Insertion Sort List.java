d3666109d7168ca97f8ef553752ed4ae395072eb
dachen <2672921697@qq.com>
2017-06-28 11:14:31 -0400

    147. Insertion Sort List

---
+++ Linked List/147-Insertion Sort List.java
@@ -0,0 +1,49 @@
+/**
+147. Insertion Sort List
+
+Sort a linked list using insertion sort.
+*/
+
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param head: The first node of linked list.
+     * @return: The head of linked list.
+     */
+    public ListNode insertionSortList(ListNode head) {
+        // write your code here
+        if (head == null || head.next == null) {
+            return head; 
+        }
+        
+        ListNode dummy = new ListNode(Integer.MIN_VALUE); 
+        ListNode pre = null; 
+        ListNode ite = dummy; 
+        ListNode temp; 
+        
+        while (head != null) {
+            while (ite != null && head.val >= ite.val) {
+                pre = ite; 
+                ite = ite.next; 
+            }
+            pre.next = head; 
+            temp = head.next; 
+            head.next = ite; 
+            head = temp;   
+            ite = dummy; 
+            pre = null; 
+        }
+        
+        return dummy.next; 
+    }
+}
