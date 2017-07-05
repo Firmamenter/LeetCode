a79917675d31ee4f965fdc76f20235fbbbf71b34
dachen <2672921697@qq.com>
2017-07-05 15:17:40 -0400

    120. Triangle

---
+++ Dynamic Programming/120-Triangle.java
@@ -0,0 +1,50 @@
+/**
+120. Triangle
+
+Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
+
+For example, given the following triangle
+[
+     [2],
+    [3,4],
+   [6,5,7],
+  [4,1,8,3]
+]
+The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+
+Solution: Top down DP.
+*/
+
+public class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if (triangle == null || triangle.size() == 0 || triangle.get(0).size() == 0) {
+            return 0; 
+        }
+        List<List<Integer>> min = new ArrayList<>(); 
+        List<Integer> level = new ArrayList<>(); 
+        level.add(triangle.get(0).get(0)); 
+        min.add(level); 
+        for (int i = 1; i < triangle.size(); i++) {
+            level = new ArrayList<>(); 
+            int len = triangle.get(i).size(); 
+            for (int j = 0; j < len; j++) {
+                if (j == 0) {
+                    level.add(min.get(i - 1).get(j) + triangle.get(i).get(j)); 
+                } else if (j == len - 1) {
+                    level.add(min.get(i - 1).get(j - 1) + triangle.get(i).get(j)); 
+                } else {
+                    level.add(Math.min(min.get(i - 1).get(j - 1), min.get(i - 1).get(j)) + triangle.get(i).get(j)); 
+                }
+            }
+            min.add(level); 
+        }
+        int res = Integer.MAX_VALUE; 
+        for (int k = 0; k < min.get(min.size() - 1).size(); k++) {
+            if (res > min.get(min.size() - 1).get(k)) {
+                res = min.get(min.size() - 1).get(k); 
+            }
+        }
+        
+        return res; 
+    }
+}
