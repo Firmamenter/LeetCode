b3e4829c74fbcfba0adb341d47a38ea23e44350f
dachen <2672921697@qq.com>
2017-07-10 10:02:40 -0400

    53. Maximum Subarray

---
+++ Dynamic Programming/53-Maximum Subarray.java
@@ -0,0 +1,27 @@
+/**
+53. Maximum Subarray
+
+Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+
+For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
+the contiguous subarray [4,-1,2,1] has the largest sum = 6.
+
+Solution: DP.
+*/
+
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return Integer.MIN_VALUE; 
+        }
+        int max = nums[0]; 
+        int[] dp = new int[2]; 
+        dp[0] = nums[0]; 
+        for (int i = 1; i < nums.length; i++) {
+            dp[1] = Math.max(dp[0], 0) + nums[i]; 
+            dp[0] = dp[1]; 
+            max = Math.max(max, dp[0]); 
+        }
+        return max; 
+    }
+}
