de25cd256bc43340e78fbe8eaae955fd6ddcbb5f
dachen <2672921697@qq.com>
2017-07-11 10:26:34 -0400

    46. Permutations

---
+++ Graph & Search/46-Permutations.java
@@ -0,0 +1,45 @@
+/**
+46. Permutations
+
+Given a collection of distinct numbers, return all possible permutations.
+
+For example,
+[1,2,3] have the following permutations:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+
+Solution: DFS, permutation template. All leaf nodes are results.
+*/
+
+public class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>(); 
+        if (nums == null || nums.length == 0) {
+            return res; 
+        }
+        List<Integer> list = new ArrayList<>(); 
+        helper(res, nums, list); 
+        return res; 
+    }
+    
+    private void helper(List<List<Integer>> res, int[] nums, List<Integer> list) {
+        if (list.size() == nums.length) {
+            res.add(new ArrayList<Integer>(list)); 
+            return; 
+        }
+        
+        for (int num : nums) {
+            if (!list.contains(num)) {
+                list.add(num); 
+                helper(res, nums, list); 
+                list.remove(list.size() - 1); 
+            }
+        }
+    }
+}
