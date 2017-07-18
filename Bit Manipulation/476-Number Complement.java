e2983834e0d692b2e5a8a8481ff12987392beefc
dachen <2672921697@qq.com>
2017-07-18 10:40:07 -0400

    476. Number Complement

---
+++ Bit Manipulation/476-Number Complement.java
@@ -0,0 +1,31 @@
+/**
+476. Number Complement
+
+Given a positive integer, output its complement number. The complement strategy is to flip the bits of its binary representation.
+
+Note:
+The given integer is guaranteed to fit within the range of a 32-bit signed integer.
+You could assume no leading zero bit in the integer’s binary representation.
+Example 1:
+Input: 5
+Output: 2
+Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need to output 2.
+Example 2:
+Input: 1
+Output: 0
+Explanation: The binary representation of 1 is 1 (no leading zero bits), and its complement is 0. So you need to output 0.
+
+Solution: Bit manipulation.
+*/
+
+public class Solution {
+    public int findComplement(int num) {
+        int temp = num; 
+        int count = 0; 
+        while (temp != 0) {
+            count++; 
+            temp = temp >>> 1; 
+        }
+        return (int)(Math.pow(2, count) - 1) ^ num; 
+    }
+}
