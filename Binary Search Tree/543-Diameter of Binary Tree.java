84d5201f611a941ecdb82616089897c16f8980a1
dachen <2672921697@qq.com>
2017-07-21 13:06:39 -0400

    543. Diameter of Binary Tree

---
+++ Binary Search Tree/543-Diameter of Binary Tree.java
@@ -0,0 +1,62 @@
+/**
+543. Diameter of Binary Tree
+
+Given a binary tree, you need to compute the length of the diameter of the tree. The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+
+Example:
+Given a binary tree 
+          1
+         / \
+        2   3
+       / \     
+      4   5    
+Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
+
+Note: The length of path between two nodes is represented by the number of edges between them.
+
+Solution: DC.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    private class ResultType {
+        int leftLen; 
+        int rightLen; 
+        int max; 
+        ResultType(int leftLen, int rightLen, int max) {
+            this.leftLen = leftLen; 
+            this.rightLen = rightLen; 
+            this.max = max; 
+        }
+    }
+    
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (root == null) {
+            return 0; 
+        }
+        return helper(root).max - 1; 
+    }
+    
+    private ResultType helper(TreeNode root) {
+        if (root == null) {
+            return new ResultType(0, 0, 0); 
+        }
+        
+        //Divide 
+        ResultType left = helper(root.left); 
+        ResultType right = helper(root.right); 
+        
+        //Combine 
+        int maxLen = Math.max(left.leftLen, left.rightLen) + 1 + Math.max(right.leftLen, right.rightLen); 
+        maxLen = Math.max(left.max, Math.max(right.max, maxLen)); 
+        return new ResultType(Math.max(left.leftLen, left.rightLen) + 1, Math.max(right.leftLen, right.rightLen) + 1, maxLen); 
+    }
+}
