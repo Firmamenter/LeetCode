e82cacc276d5716f104c6fc695fe2b56d0f3b65a
dachen <2672921697@qq.com>
2017-08-07 15:40:04 -0400

    75. Sort Colors

--- Two Pointers/75. Sort Colors.java
+++ Two Pointers/75. Sort Colors.java
@@ -1,5 +1,5 @@
 /**
-LintCode: Sort Colors
+75. Sort Colors
 
 Given an array with n objects colored red, white or blue, sort them so that objects of the same color are adjacent, with the colors in the order red, white and blue.
 
