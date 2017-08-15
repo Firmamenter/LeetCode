bb41acee84f949323dda32572e08b1347dea8752
dachen <2672921697@qq.com>
2017-08-15 15:51:18 -0400

    384. Shuffle an Array

---
+++ Others/384-Shuffle an Array.java
@@ -0,0 +1,73 @@
+/**
+384. Shuffle an Array
+
+Shuffle a set of numbers without duplicates.
+
+Example:
+
+// Init an array with set 1, 2, and 3.
+int[] nums = {1,2,3};
+Solution solution = new Solution(nums);
+
+// Shuffle the array [1,2,3] and return its result. Any permutation of [1,2,3] must equally likely to be returned.
+solution.shuffle();
+
+// Resets the array back to its original configuration [1,2,3].
+solution.reset();
+
+// Returns the random shuffling of array [1,2,3].
+solution.shuffle();
+
+Solution: Reservoir sampling. 
+*/
+
+public class Solution {
+    private int[] org; 
+    
+    public Solution(int[] nums) {
+        org = new int[nums.length]; 
+        for (int i = 0; i < nums.length; i++) {
+            org[i] = nums[i]; 
+        }
+    }
+    
+    /** Resets the array to its original configuration and return it. */
+    public int[] reset() {
+        return org; 
+    }
+    
+    /** Returns a random shuffling of the array. */
+    public int[] shuffle() {
+        int[] res = new int[org.length]; 
+        List<Integer> list = new ArrayList<>(); 
+        for (int i = 0; i < org.length; i++) {
+            list.add(org[i]); 
+        }
+        for (int i = 0; i < org.length; i++) {
+            res[i] = sampling(list); 
+        }
+        return res; 
+    }
+    
+    private int sampling(List<Integer> list) {
+        int count = 1; 
+        int flag = list.get(0); 
+        int pos = 0; 
+        for (int i = 0; i < list.size(); i++) {
+            if (Math.random() < 1.0 / count) {
+                flag = list.get(i); 
+                pos = i; 
+            }
+            count++; 
+        }
+        list.remove(pos); 
+        return flag; 
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int[] param_1 = obj.reset();
+ * int[] param_2 = obj.shuffle();
+ */
