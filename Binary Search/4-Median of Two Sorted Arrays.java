80d9bf96b62cffb830068d3431dfe61e801954e7
dachen <2672921697@qq.com>
2018-05-19 23:00:53 -0400

    update 4

--- Binary Search/4-Median of Two Sorted Arrays.java
+++ Binary Search/4-Median of Two Sorted Arrays.java
@@ -54,3 +54,39 @@ public class Solution {
         }
     }
 }
+
+
+public class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if (nums1 == null && nums2 == null || nums1.length == 0 && nums2.length == 0) {
+            return 0; 
+        }
+        int length = nums1.length + nums2.length; 
+        if (length % 2 == 0) {
+            return (findKthElement(nums1, nums2, length / 2, 0, 0) + findKthElement(nums1, nums2, length / 2 + 1, 0, 0)) / 2.0; 
+        } else {
+            return findKthElement(nums1, nums2, length / 2 + 1, 0, 0); 
+        }
+    }
+    
+    public double findKthElement(int[] A, int[] B, int k, int startA, int startB) {
+        if (k == 1) {
+            if (startA >= A.length) {
+                return B[startB]; 
+            }
+            if (startB >= B.length) {
+                return A[startA]; 
+            }
+            return A[startA] > B[startB] ? B[startB] : A[startA]; 
+        }
+        
+        int keyA = k / 2 - 1 + startA < A.length ? A[k / 2 - 1 + startA] : Integer.MAX_VALUE; 
+        int keyB = k / 2 - 1 + startB < B.length ? B[k / 2 - 1 + startB] : Integer.MAX_VALUE; 
+        
+        if (keyA < keyB) {
+            return findKthElement(A, B, k - k / 2, startA + k / 2, startB);
+        } else {
+            return findKthElement(A, B, k - k / 2, startA, startB + k / 2);    
+        }
+    }
+}
