b4b0216882832af52783cea968f1ca3fb81b7263
dachen <2672921697@qq.com>
2018-11-06 18:52:00 -0800

    466

--- Dynamic Programming/466-Count The Repetitions.java
+++ Dynamic Programming/466-Count The Repetitions.java
@@ -18,3 +18,35 @@ Return:
 
 Sol: DP. 
 */
+class Solution {
+    public int getMaxRepetitions(String s1, int n1, String s2, int n2) {
+        if (n1 == 0) {
+            return 0; 
+        }
+        int[] repeatCnt = new int[s2.length() + 1]; 
+        int[] idx = new int[s2.length() + 1]; 
+        int nextIdx = 0; 
+        int cnt = 0; 
+        for (int i = 0; i < n1; i++) {
+            for (int j = 0; j < s1.length(); j++) {
+                if (s1.charAt(j) == s2.charAt(nextIdx)) {
+                    if (nextIdx++ == s2.length() - 1) {
+                        cnt++; 
+                    }
+                    nextIdx %= s2.length(); 
+                }
+            }
+            repeatCnt[i] = cnt; 
+            idx[i] = nextIdx; 
+            for (int k = 0; k < i; k++) {
+                if (idx[i] == idx[k]) {
+                    int preK = repeatCnt[k]; 
+                    int pattern = (repeatCnt[i] - repeatCnt[k]) * ((n1 - 1 - k) / (i - k)); 
+                    int remaining = repeatCnt[k + (n1 - 1 - k) % (i - k)] - repeatCnt[k]; 
+                    return (preK + pattern + remaining) / n2; 
+                }
+            }
+        }
+        return repeatCnt[n1 - 1] / n2; 
+    }
+}
