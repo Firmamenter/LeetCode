5b9d31a3b7b62405713b1e1fe9f3a1133fec2330
dachen <2672921697@qq.com>
2018-11-08 11:27:36 -0800

    encircular

--- Math/Encircular.java
+++ Math/Encircular.java
@@ -4,6 +4,8 @@ Encircular
 给你一串指令 command，G走一格，R右转，L左转。指令无限循环，判断机器人是否只在某个圈内不会走出去。
 
 Sol: 走四次指令，走完每一次指令后判断坐标和方向，与起始时一样就可以。
+原因: 无论执行一次command后方向是什么，在四次执行command之后，方向一定和第一次执行command后一样；
+    接下来判断location，如果四次后location和第一次不一样，那么继续执行，将会永远都不一样，形成不了circle
 */
 
 public String encircular(String command) {
